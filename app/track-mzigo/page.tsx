f7138601cffb87bcd65e9268ecaa347a0b6add36
1-ken <kennicmwimu@gmial.com>
2025-09-09 12:42:12 +0300

    tracking mzigo

--- app/track-mzigo/page.tsx
+++ app/track-mzigo/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import SearchBar from '../components/SearchBar'
 
 const page = () => {
   return (
-    <div>
-      track mzigo
-    </div>
+    <div className="w-full flex justify-center mt-8">
+        <SearchBar placeholder="Enter the tracking number"/>        
+      </div>
   )
 }
 
